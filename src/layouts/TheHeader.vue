4d6b85f00dfefd20dc282c4121f82cba8dc8e2f2
Hye-Kyung9 <rngprud123@naver.com>
2024-06-21 10:52:24 +0900

    feat: 게시판 UI생성

--- src/layouts/TheHeader.vue
+++ src/layouts/TheHeader.vue
@@ -14,8 +14,8 @@
         >
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
+        <div class="collapse navbar-collapse" id="navbarSupprotedContent">
+          <ul class="navbar-nav me-auto">
             <li class="nav-item">
               <RouterLink class="nav-link" aria-current="page" active-class="active" to="/"
                 >Home</RouterLink
@@ -24,13 +24,30 @@
             <li class="nav-item">
               <RouterLink class="nav-link" active-class="active" to="/about">About</RouterLink>
             </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" aria-current="page" active-class="active" to="/posts"
+                >게시글
+              </RouterLink>
+            </li>
           </ul>
+          <div class="d-flex">
+            <button class="btn btn-outline-light" type="button" @click="goPage">글쓰기</button>
+          </div>
         </div>
       </div>
     </nav>
   </header>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const goPage = () => {
+  router.push({
+    name: 'PostCreate'
+  });
+};
+</script>
 
 <style lang="scss" scoped></style>
